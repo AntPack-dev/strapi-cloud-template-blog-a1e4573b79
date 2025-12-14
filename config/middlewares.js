25f148b85b602b8a7670913846d345d2c0d90350
andresft123 <andres.fajardo@antpack.co>
2025-12-14 13:34:33 -0500

    fix images

--- config/middlewares.js
+++ config/middlewares.js
@@ -11,40 +11,57 @@
 //   'strapi::public',
 // ];
 
-module.exports = ({ env }) => [
-  'strapi::logger',
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
-            env('RESOURCES_CDN'),
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
-            env('RESOURCES_CDN'),
-          ],
-          upgradeInsecureRequests: null,
+module.exports = ({ env }) => {
+  // Construir lista de fuentes permitidas para imágenes y medios
+  const imgSrc = [
+    "'self'",
+    'data:',
+    'blob:',
+    // S3 bucket directo
+    `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+    // CDN personalizado
+    env('RESOURCES_CDN'),
+    // Strapi Cloud media storage (para recursos cargados antes de configurar S3)
+    'https://*.media.strapiapp.com',
+    '*.media.strapiapp.com',
+  ].filter(Boolean); // Eliminar valores undefined/null
+
+  const mediaSrc = [
+    "'self'",
+    'data:',
+    'blob:',
+    // S3 bucket directo
+    `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+    // CDN personalizado
+    env('RESOURCES_CDN'),
+    // Strapi Cloud media storage (para recursos cargados antes de configurar S3)
+    'https://*.media.strapiapp.com',
+    '*.media.strapiapp.com',
+  ].filter(Boolean); // Eliminar valores undefined/null
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': imgSrc,
+            'media-src': mediaSrc,
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
