c0c43beff896032cda073afd4af82fc037fcc4e3
marioantpack <mario.villegas@antpack.co>
2026-03-03 14:24:07 -0500

    Add comment

--- src/api/comment/routes/comment.js
+++ src/api/comment/routes/comment.js
@@ -4,6 +4,42 @@ module.exports = {
   type: 'content-api',
   routes: [
     {
+      method: 'POST',
+      path: '/comments',
+      handler: 'comment.create',
+      config: {
+        policies: [],
+        middlewares: [],
+        auth: {
+          scope: []
+        }
+      }
+    },
+    {
+      method: 'PUT',
+      path: '/comments/:id',
+      handler: 'comment.update',
+      config: {
+        policies: [],
+        middlewares: [],
+        auth: {
+          scope: []
+        }
+      }
+    },
+    {
+      method: 'DELETE',
+      path: '/comments/:id',
+      handler: 'comment.delete',
+      config: {
+        policies: [],
+        middlewares: [],
+        auth: {
+          scope: []
+        }
+      }
+    },
+    {
       method: 'GET',
       path: '/comments/article/:articleId',
       handler: 'comment.getCommentsByArticle',
