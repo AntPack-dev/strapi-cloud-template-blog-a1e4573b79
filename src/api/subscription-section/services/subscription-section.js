743cbb379cc4ef927c2195d074003091f1049297
Sebastián Meneses <sebastian.meneses@antpack.co>
2026-03-05 15:52:42 -0500

    add: single type of subscriptionSection

---
+++ src/api/subscription-section/services/subscription-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * subscription-section service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::subscription-section.subscription-section');
