b4114ea237d7c1965c1c94d0f30f6026c4ae071d
Sebastián Meneses <sebastian.meneses@antpack.co>
2026-06-01 17:49:25 -0500

    fix(cors): configure CORS via CORS_ALLOWED_ORIGINS env variable

--- config/middlewares.js
+++ config/middlewares.js
@@ -39,9 +39,17 @@ module.exports = ({ env }) => {
     {
       name: 'strapi::cors',
       config: {
-        origin: ['http://localhost:3000', 'http://localhost:1337', 'http://localhost:8080'],
+        enabled: true,
+        origin: env('CORS_ALLOWED_ORIGINS')
+          ? env('CORS_ALLOWED_ORIGINS').split(',')
+          : [
+              'http://localhost:3000',
+              'http://localhost:1337',
+              'http://localhost:8080',
+            ],
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-        headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+        headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'x-nonce'],
+        keepHeaderOnError: true,
         credentials: true,
       },
     },
